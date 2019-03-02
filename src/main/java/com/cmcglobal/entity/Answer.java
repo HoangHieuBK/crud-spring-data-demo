8e69f819a5d91e8667da64f31eee1b310a8e768b
Bui Hoang Hieu <hoanghieubk1996@gmail.com>
2019-03-02 11:35:14 +0700

    submit

---
+++ src/main/java/com/cmcglobal/entity/Answer.java
@@ -0,0 +1,97 @@
+package com.cmcglobal.entity;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name="answer", schema = "TESTING_SYSTEM_DATABASE")
+
+public class Answer {
+
+	@Id
+	@Column(name = "answer_id", nullable = false, unique=true)
+	private  String answerId;
+	
+	@Column(name = "content")
+	private String content;
+	
+	@Column(name = "is_true")
+	private int isTrue;
+	
+	@Column(name = "status")
+	private int status;
+	
+	@ManyToOne(cascade = CascadeType.DETACH, fetch = FetchType.EAGER)
+	@JoinColumn(name = "question_id")
+	@JsonIgnoreProperties("questionAnswer")
+	Question answerQ;
+	
+	public Answer(String answerId, String content, int isTrue, int status, Question answerQ) {
+		super();
+		this.answerId = answerId;
+		this.content = content;
+		this.isTrue = isTrue;
+		this.status = status;
+		this.answerQ = answerQ;
+	}
+
+	public Answer() {
+		super();
+	}
+
+	/**
+	 * @return the answerId
+	 */
+	public String getAnswerId() {
+		return answerId;
+	}
+
+	/**
+	 * @param answerId the answerId to set
+	 */
+	public void setAnswerId(String answerId) {
+		this.answerId = answerId;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getIsTrue() {
+		return isTrue;
+	}
+
+	public void setIsTrue(int isTrue) {
+		this.isTrue = isTrue;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public Question getAnswerQ() {
+		return answerQ;
+	}
+
+	public void setAnswerQ(Question answerQ) {
+		this.answerQ = answerQ;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/cmcglobal/entity/QuestionCategory.java
@@ -0,0 +1,103 @@
+package com.cmcglobal.entity;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+@Entity
+@Table(name = "category", schema = "TESTING_SYSTEM_DATABASE")
+public class QuestionCategory {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "category_id", nullable = false, unique = true)
+	private int id;
+
+	@Column(name = "category_name")
+	private String categoryName;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "date_created")
+	private Date dateCreated;
+
+	@Column(name = "status")
+	private int status;
+
+
+	@ManyToOne(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+	@JoinColumn(name = "user_id_created")
+	User userCategory;
+
+	public QuestionCategory(int id, String categoryName, Date dateCreated, int status, User userCategory) {
+		super();
+		this.id = id;
+		this.categoryName = categoryName;
+		this.dateCreated = dateCreated;
+		this.status = status;
+		this.userCategory = userCategory;
+	}
+
+	public QuestionCategory() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public User getUserCategory() {
+		return userCategory;
+	}
+
+	public void setUserCategory(User userCategory) {
+		this.userCategory = userCategory;
+	}
+
+}
